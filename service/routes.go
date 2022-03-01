f9eca0bbfe7c5c3ba7711f1ab100a4612f3e1f9d
Andrew Gibson <andrew.gibson@armakuni.com>
2022-03-01 15:41:06 +0000

    whitespace lint

--- service/routes.go
+++ service/routes.go
@@ -19,7 +19,7 @@ func (svc *Service) publicEndpoints(ctx context.Context) {
 	log.Info(ctx, "enabling public endpoints")
 
 	// Routes
-	//areaTypes := handler.NewAreaTypes(svc.responder, svc.cantabularClient)
+	// areaTypes := handler.NewAreaTypes(svc.responder, svc.cantabularClient)
 
-	//svc.router.Get("/area-types", areaTypes.Get)
+	// svc.router.Get("/area-types", areaTypes.Get)
 }
