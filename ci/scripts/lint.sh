504fd9328446ea2c90bb1af5fb0bc8f298ae3e00
Andrew Gibson <andrew.gibson@armakuni.com>
2022-03-02 12:55:23 +0000

    update version of lint

--- ci/scripts/lint.sh
+++ ci/scripts/lint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 pushd dp-population-types-api
-  go get github.com/golangci/golangci-lint/cmd/golangci-lint@v1.43.0
+  go install github.com/golangci/golangci-lint/cmd/golangci-lint
   ls -la
   make lint
 popd
