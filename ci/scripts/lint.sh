39027b150685e137a34307f20af07a8b7a443a5d
Geraint Edwards <gedgey@gmail.com>
2022-06-13 16:56:58 +0100

    specify lint version

--- ci/scripts/lint.sh
+++ ci/scripts/lint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 pushd dp-population-types-api
-  go install github.com/golangci/golangci-lint/cmd/golangci-lint
+  go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.46.2
   go build
   ls -la
   make lint
